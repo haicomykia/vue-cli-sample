225bddbc2b9a0610f44b7b85b63ba3f61d726dda
undefinedSynthesis <undefinedsynthesis@gmail.com>
2021-10-18 02:14:14 +0900

    add original vue component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <Test></Test>
+  <HelloWorld msg="虹ヶ咲学園スクールアイドル同好会" />
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import Test from "./components/Test.vue";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
+    Test,
   },
 };
 </script>
---
+++ src/components/Test.vue
@@ -0,0 +1,21 @@
+<template>
+  <input type="text" v-model="msg">
+  <p>{{msg}}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "上原歩夢"
+    }
+  },
+}
+</script>
+
+<style scoped>
+  p {
+    font-size: 2rem;
+    color:#FFB6C1;
+  }
+</style>
